04fc1868751d8510be8ff742f4c8ed74c64b93f6
Luca <73708474+Luca-Landri@users.noreply.github.com>
2024-09-09 22:58:42 +0200

    Fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-import { motion, useScroll, useTransform } from "framer-motion";
 import Hero from "./components/hero";
 import Navbar from "./components/Navbar";
 import Problem from "./components/Problem";
@@ -11,36 +9,18 @@ import Footer from "./components/footer";
 import AboutUs from "./components/aboutUs";
 
 export default function HomePage() {
-  const { scrollY } = useScroll();
-
-  // Parallax effect for Problem (page moves up)
-  const yProblem = useTransform(scrollY, [0, 0], [0, 0]);
-
-  // Normal scroll for Solution onwards
-  const ySolution = useTransform(scrollY, [600, 1500], [0, 0]);
-
   return (
     <div className="bg-gradient-to-r from-[#050520] to-[#0a0a40] overflow-hidden">
       <Navbar />
       <Hero />
-      
-      {/* Problem section with inverse parallax */}
-      <motion.div style={{ y: yProblem }}>
-        <Problem />
-      </motion.div>
-      
-      {/* Solution section with normal scrolling */}
-      <motion.div
-        className="bg-gradient-to-r from-[#1a1a60] to-[#2a2a80] rounded-[45px]"
-        style={{ y: ySolution }}
-      >
+      <Problem />
+      <div className="bg-gradient-to-r from-[#1a1a60] to-[#2a2a80] rounded-[45px]">
         <Solution />
         <Versus />
         <WhyThis />
         <WhyNow />
         <AboutUs />
-      </motion.div>
-      
+      </div>
       <Footer />
     </div>
   );
